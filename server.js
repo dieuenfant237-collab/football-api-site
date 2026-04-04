4bd60765a4edb45a41fa05deab6bb909c647ae2b
dieuenfant237-collab <dieuenfant237@gmail.com>
2026-04-04 12:39:00 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 
 app.get('/matches', async (req, res) => {
   try {
-    const response = await axios.get('https://v3.football.api-sports.io/fixtures?live=all', {
+    const response = await axios.get('https://v3.football.api-sports.io/fixtures?live=all',, {
       headers: {
         'x-apisports-key': '1a75b32YmFdbseEwfMCsY3lcuycW8DRN1BULcaQOBmy3FqX32UCBmwruz6S5
       }
