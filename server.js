57077474c2f0703fbbbee176d8721e3f2e2ede59
dieuenfant237-collab <dieuenfant237@gmail.com>
2026-04-04 11:11:56 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('API Football fonctionne 🚀');
 });
 
+app.get('/matches', (req, res) => {
+  res.send('Route matchs OK ⚽');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
